ae4cf13282065dd0beaabccbaf2aa59de837d7f2
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 02:14:45 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "b034f06b-49f4-4cf9-9185-c9e85d9e57b6",
+    "home_feed": [
+        {
+            "id": "0503cced-47d7-438d-a348-f053e499039d",
+            "artist_name": "industry",
+            "home_carousel": [
+                {
+                    "id": "b58ba82e-68a0-43a9-8524-7148dc2ead1d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "b424d8a0-1885-4868-a8ad-3bdf39e40802",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                },
+                {
+                    "id": "43451f21-c1c3-4006-b3eb-01e7a8821ccc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "d89952a7-f587-4ed3-932a-d2bf5aec6828",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "71ad6e10-1706-4a0d-8781-662a123ba12f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                }
+            ],
+            "art_caption": "suggest",
+            "art_description": "Voice cut process similar high once. Probably put result political win oil head."
+        },
+        {
+            "id": "6b2cfe62-790c-4c9f-a5be-a401374de1e7",
+            "artist_name": "group",
+            "home_carousel": [
+                {
+                    "id": "72314eb0-f5f4-429b-8ee7-eb6db43d923f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "bd8ad126-4bf5-44c4-a98a-0bb35852342e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "b696d6c1-f76e-4ca1-b311-ec5bc3a5d035",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                },
+                {
+                    "id": "2fcd5714-d37f-40e2-8ef1-76618ffb072b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "2bec0af1-3826-4893-927c-8c46ff5af827",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                }
+            ],
+            "art_caption": "himself",
+            "art_description": "Century determine I value. Memory find name cover way."
+        },
+        {
+            "id": "2c1a2b9b-ff80-4792-9031-a874e94d7940",
+            "artist_name": "physical",
+            "home_carousel": [
+                {
+                    "id": "f84256ba-ca53-48a9-b115-1d361bd98074",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png"
+                },
+                {
+                    "id": "19a13120-bce0-4d87-80d7-43d8e0338d1f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "4d03712d-bd1e-4977-a32b-d8ecf52d8bc9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "366e86b0-1a28-44e4-8dbb-f048ce1ac526",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "7c728cca-991d-4f96-9e2a-4ba213a6e408",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "executive",
+            "art_description": "Which factor police condition employee heavy fear. General position source need movement decide tough brother. Vote environmental first huge positive consider scientist lose."
+        },
+        {
+            "id": "fe292d72-36e4-4d6e-a1aa-cbcccde71dd5",
+            "artist_name": "fund",
+            "home_carousel": [
+                {
+                    "id": "a779395f-a3ef-4dbc-8af9-eed5afc664cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "329fe627-58b1-4153-bfa6-a939fb33816a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "cea018b1-b8d3-4258-9fc5-9bf30c2cac86",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "d500b643-c02a-4b9b-89b7-3964117f110e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "7d00cf3b-14f8-48c9-8c87-807876867c24",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                }
+            ],
+            "art_caption": "serve",
+            "art_description": "Attorney sound watch deep despite while hotel."
+        },
+        {
+            "id": "da85dc57-8fd1-441f-a913-53a7dbb1bacc",
+            "artist_name": "appear",
+            "home_carousel": [
+                {
+                    "id": "a592d91d-ac5e-4a9b-9fbe-0995912b20ee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "4de345d8-5730-419c-b523-fb9e1017aec7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "c0cdbd06-7414-4af3-852d-421d88fc74aa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "af99ec85-7b81-4299-9f2e-307a17636b68",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "f89fe0d4-effd-4e2e-a402-d4ed8366cfcb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                }
+            ],
+            "art_caption": "mother",
+            "art_description": "Since parent put. Rich describe top region strong party decision."
+        }
+    ]
+}
